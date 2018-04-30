f98e01542fb7fffe3c2997cf6a152b387b813e95
Francesco Rumiano <francesco.rumiano@aol.com>
2018-04-30 02:55:43 +0200

    esercitazione_0_1.m just started

---
+++ 01/esercitazione_0_1.m
@@ -0,0 +1,17 @@
+%%
+clear
+close
+clc
+
+disp('***** ESERCIZIO 1 *****')
+
+disp('N=10          t=5')
+disp('a=1.483593
+
+disp('***** FINE *****')
+pause
+%%
+clc
+
+disp('***** ESERCIZIO 2 *****')
+
